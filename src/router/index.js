ed2fad01ea90c031e0fef01e9706d9f747d8eb41
Ahmed Hassan <ah2k.dev@gmail.com>
2023-11-03 23:00:40 +0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 const auth = require("./auth");
 const discussion = require("./discussion");
+const contactUs = require("./contactUs");
 const router = require("express").Router();
 
 router.use("/auth", auth);
 router.use("/discussion", discussion);
+router.use("/contactUs", contactUs);
 
 module.exports = router;
